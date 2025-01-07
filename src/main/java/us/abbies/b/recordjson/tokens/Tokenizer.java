eca0dfbe1a0b1089f836a3a4f398fdfbc1101e90
Steve McKay <steve@b.abbies.us>
2025-01-07 10:38:54 -0600

    tests pass

--- src/main/java/us/abbies/b/recordjson/tokens/Tokenizer.java
+++ src/main/java/us/abbies/b/recordjson/tokens/Tokenizer.java
@@ -7,10 +7,11 @@ import java.util.NoSuchElementException;
 
 public class Tokenizer implements Iterator<Token> {
     private final Reader input;
-    private int line;
-    private int character;
+    private int line =  1;
+    private int column;
     private String errorMessage;
     private Token onDeck;
+    private boolean hitError;
 
     Tokenizer(Reader input) {
         this.input = input;
@@ -18,7 +19,9 @@ public class Tokenizer implements Iterator<Token> {
 
     @Override
     public boolean hasNext() {
-        if (onDeck != null) {
+        if (hitError) {
+            return false;
+        } else if (onDeck != null) {
             return true;
         } else {
             tryReadToken();
@@ -28,15 +31,20 @@ public class Tokenizer implements Iterator<Token> {
 
     @Override
     public Token next() {
+        if (hitError) {
+            throw new NoSuchElementException();
+        }
         if (onDeck == null) {
             tryReadToken();
         }
-
         if (onDeck == null) {
             throw new NoSuchElementException();
         }
 
         Token result = onDeck;
+        if (result.type() == TokenType.ERROR) {
+            hitError = true;
+        }
         onDeck = null;
         return result;
     }
@@ -48,36 +56,49 @@ public class Tokenizer implements Iterator<Token> {
 
         int c = discardWhitespace();
         onDeck = switch (c) {
-            case '{' -> new Token(TokenType.OBJ_START, null, line, character);
-            case '}' -> new Token(TokenType.OBJ_END, null, line, character);
-            case ':' -> new Token(TokenType.OBJ_NAME_SEP, null, line, character);
-            case ',' -> new Token(TokenType.OBJ_VAL_SEP, null, line, character);
-            case '[' -> new Token(TokenType.ARRAY_START, null, line, character);
-            case ']' -> new Token(TokenType.ARRAY_END, null, line, character);
+            case '{' -> new Token(TokenType.OBJ_START, null, line, column);
+            case '}' -> new Token(TokenType.OBJ_END, null, line, column);
+            case ':' -> new Token(TokenType.OBJ_NAME_SEP, null, line, column);
+            case ',' -> new Token(TokenType.OBJ_VAL_SEP, null, line, column);
+            case '[' -> new Token(TokenType.ARRAY_START, null, line, column);
+            case ']' -> new Token(TokenType.ARRAY_END, null, line, column);
             case '"' -> {
                 // TODO: read string
-                yield new Token(TokenType.ERROR, "Strings unimplemented", line, character);
+                yield new Token(TokenType.ERROR, "Strings unimplemented", line, column);
             }
-            case 'f' -> {
-                // TODO: read false
-                yield new Token(TokenType.ERROR, "Booleans unimplemented", line, character);
-            }
-            case 'n' -> {
-                // TODO: read null
-                yield new Token(TokenType.ERROR, "Nulls unimplemented", line, character);
-            }
-            case 't' -> {
-                // TODO: read true
-                yield new Token(TokenType.ERROR, "Booleans unimplemented", line, character);
-            }
+            case 'f' -> expect("alse", new Token(TokenType.LIT_BOOL, false, line, column));
+            case 'n' -> expect("ull", new Token(TokenType.LIT_NULL, null, line, column));
+            case 't' -> expect("rue", new Token(TokenType.LIT_BOOL, true, line, column));
             case '-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' -> {
                 // TODO: read number
-                yield new Token(TokenType.ERROR, "Numbers unimplemented", line, character);
+                yield new Token(TokenType.ERROR, "Numbers unimplemented", line, column);
             }
             case -1 -> null;
-            case -2 -> new Token(TokenType.ERROR, errorMessage, line, character);
-            default -> new Token(TokenType.ERROR, "Unrecognized character: " + Character.toString(c), line, character);
+            case -2 -> new Token(TokenType.ERROR, errorMessage, line, column);
+            default -> new Token(TokenType.ERROR, "Unrecognized character: " + Character.toString(c), line, column);
         };
+    }
+
+    private Token expect(String remaining, Token successToken) {
+        for (char expected : remaining.toCharArray()) {
+            int actual;
+            try {
+                actual = input.read();
+            } catch (IOException e) {
+                return new Token(TokenType.ERROR, e.getMessage(), line, column);
+            }
+
+            if (actual == -1) {
+                return new Token(TokenType.ERROR, "Unexpected end of input", line, column);
+            }
+
+            column++;
+            if (expected != actual) {
+                return new Token(TokenType.ERROR, "Unexpected character: " + Character.toString(actual), line, column);
+            }
+        }
+
+        return successToken;
     }
 
     private int discardWhitespace() {
@@ -92,9 +113,9 @@ public class Tokenizer implements Iterator<Token> {
 
             if (result == 0x0A) {
                 line++;
-                character = 0;
+                column = 0;
             } else {
-                character++;
+                column++;
             }
         } while (result == 0x20 || result == 0x09 || result == 0x0A || result == 0x0D);
         return result;
--- src/test/java/us/abbies/b/recordjson/tokens/TokenizerTest.java
+++ src/test/java/us/abbies/b/recordjson/tokens/TokenizerTest.java
@@ -1,9 +1,7 @@
 package us.abbies.b.recordjson.tokens;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.FieldSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.StringReader;
@@ -15,12 +13,20 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class TokenizerTest {
+    static Stream<Arguments> nullTestCases() {
+        return Stream.of(
+                arguments("null", List.of(new Token(TokenType.LIT_NULL, null, 1, 1))),
+                arguments("nll", List.of(new Token(TokenType.ERROR, "Unexpected character: l", 1, 2))),
+                arguments("nul", List.of(new Token(TokenType.ERROR, "Unexpected end of input", 1, 3)))
+        );
+    }
+
     static Stream<Arguments> booleanTestCases() {
         return Stream.of(
                 arguments("false", List.of(new Token(TokenType.LIT_BOOL, false, 1, 1))),
                 arguments("true", List.of(new Token(TokenType.LIT_BOOL, true, 1, 1))),
-                arguments("flase", List.of(new Token(TokenType.ERROR, "Unrecognized character: l", 1, 2))),
-                arguments("ture", List.of(new Token(TokenType.ERROR, "Unrecognized character: u", 1, 2))),
+                arguments("flase", List.of(new Token(TokenType.ERROR, "Unexpected character: l", 1, 2))),
+                arguments("ture", List.of(new Token(TokenType.ERROR, "Unexpected character: u", 1, 2))),
                 arguments("f", List.of(new Token(TokenType.ERROR, "Unexpected end of input", 1, 1))),
                 arguments("fals", List.of(new Token(TokenType.ERROR, "Unexpected end of input", 1, 4))),
                 arguments("t", List.of(new Token(TokenType.ERROR, "Unexpected end of input", 1, 1))),
@@ -29,7 +35,7 @@ class TokenizerTest {
     }
 
     @ParameterizedTest(name = "{0}")
-    @MethodSource("booleanTestCases")
+    @MethodSource({"booleanTestCases", "nullTestCases"})
     public void runTests(String input, List<Token> expectedTokens) {
         var tokenizer = new Tokenizer(new StringReader(input));
         List<Token> actualTokens = new ArrayList<>();
